ed58be9c39d74e668acdfcd65c9519ececbd31de
Arvin Lleva <arvin.lleva@yahoo.com>
2020-01-19 21:35:28 -0800

    added the get delete and post endpoints for returning an exercise, deleting and exercise, and updating and exercise

--- backend/routes/exercises.js
+++ backend/routes/exercises.js
@@ -9,7 +9,7 @@ router.route('/').get((req,res) => {
 
 router.route('/add').post((req, res) => {
   const username = req.body.username
-  const description = req.body.desciprtion
+  const description = req.body.description
   const duration = Number(req.body.duration) // this converts it into a number
   const date = Date.parse(req.body.date) // converting to the date datatype
 
@@ -21,9 +21,37 @@ router.route('/add').post((req, res) => {
   })
 
   // this allows the data to persists in the data base
-  newExercise.new()
+  newExercise.save()
     .then(() => res.json('Exercise added!'))
     .catch(err => res.status(400).json('Error: ' + err))
 }) 
+
+router.route('/:id').get((req, res) => {
+  Exercise.findById(req.params.id)
+    .then(exercise => res.json(exercise))
+    .catch(err => res.status(400).json('Error: ' + err))
+      
+})
+
+router.route('/:id').delete((req,res) => {
+  Exercise.findByIdAndDelete(req.params.id)
+    .then(() => res.json('Exercise deleted.'))
+    .catch(err => res.status(400).json('Error: ' + err))
+})
+
+router.route('/update/:id').post((req, res) => {
+  Exercise.findById(req.params.id)
+    .then(exercise => {
+      exercise.username = req.body.username
+      exercise.description = req.body.description
+      exercise.duration = Number(req.body.duration)
+      exercise.date = Date.parse(req.body.date)
+      
+      exercise.save() 
+        .then(() => res.json('Exercise updated!'))
+        .catch(err => res.status(400).json('Error: ' + err))
+    })
+    .catch(err => res.status(400).json('Error: ' + err))
+})
 
 module.exports = router
