699a6bbb3dab6462a3e21ed60898f50b66ee366d
Arvin Lleva <arvin.lleva@yahoo.com>
2020-01-19 22:48:33 -0800

    implementing the state or the hooks for creating an exercise

--- frontend/src/components/create-exercise.component.js
+++ frontend/src/components/create-exercise.component.js
@@ -1,6 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 
+/* This component allows us to add data to the database*/
 function CreateExercise() {
+  const [username, setUsername] = useState('')
+  const [description, setDescription] = useState('')
+  const [duration, setDuration] = useState(0)
+  const [date, setDate] = useState(new Date())
+  const [users, setUsers] = useState([])
+
   return (
     <div>
       <h1>Create Exercise</h1>
